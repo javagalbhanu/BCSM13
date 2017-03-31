e3016f83f9f6c1b17dd4e69ff12541fd6710d65a
javagalbhanu <javagalbhanu@gmail.com>
2017-03-31 18:08:57 +0530

    Grid

--- src/qsp/Demo.java
+++ src/qsp/Demo.java
@@ -1,15 +1,20 @@
 package qsp;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class Demo {
 
 	@Test
-	public void testDemo(){
-		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+	public void testDemo() throws MalformedURLException{
+		URL node=new URL("http://localhost:4444/wd/hub");
+		WebDriver driver=new RemoteWebDriver(node,DesiredCapabilities.chrome());
 		driver.get("http://www.google.com");
 		System.out.println(driver.getTitle());
 //		driver.quit();
