4b1b06b8e0430b42dfb375094f4ad1b48a5bc169
javagalbhanu <javagalbhanu@gmail.com>
2017-03-31 17:49:07 +0530

    Demo

---
+++ src/qsp/Demo.java
@@ -0,0 +1,16 @@
+package qsp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void testDemo(){
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.google.com");
+		driver.quit();
+	}
+}
